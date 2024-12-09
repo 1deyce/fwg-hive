cb7dd05a078f827a1ff088680a06cc9c639d8255
1deyce <124916049+1deyce@users.noreply.github.com>
2024-12-09 13:58:10 +0200

    Update uploadAvatar.tsx

--- app/dashboard/profile/uploadAvatar.tsx
+++ app/dashboard/profile/uploadAvatar.tsx
@@ -1,48 +1,48 @@
-"use client";
+// "use client";
 
-import { Button } from "@/components/ui/button";
-import { useToast } from "@/hooks/use-toast";
-import { useState } from "react";
-import useUserStore from "@/zustand/store/userStore";
-import { uploadFile } from "@/app/actions";
+// import { Button } from "@/components/ui/button";
+// import { useToast } from "@/hooks/use-toast";
+// import { useState } from "react";
+// import useUserStore from "@/zustand/store/userStore";
+// import { uploadFile } from "@/app/actions";
 
-export default function UploadAvatar() {
-    const [avatar, setAvatar] = useState<File[] | null>(null);
-    const { toast } = useToast();
-    const { getUser } = useUserStore();
-    const user = getUser();
+// export default function UploadAvatar() {
+//     const [avatar, setAvatar] = useState<File[] | null>(null);
+//     const { toast } = useToast();
+//     const { getUser } = useUserStore();
+//     const user = getUser();
 
-    if (!user) throw new Error("User not found for avatar upload");
+//     if (!user) throw new Error("User not found for avatar upload");
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-        if (files && files.length > 0) {
-            const file = files[0];
-            setAvatar([file]);
-            console.log("Files selected:", file);
-        } else {
-            setAvatar(null);
-        }
-    };
+//     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//         const files = event.target.files;
+//         if (files && files.length > 0) {
+//             const file = files[0];
+//             setAvatar([file]);
+//             console.log("Files selected:", file);
+//         } else {
+//             setAvatar(null);
+//         }
+//     };
 
-    return (
-        <div>
-            <form action={uploadFile}>
-                <input
-                    name="file"
-                    type="file"
-                    accept="image/*"
-                    onChange={handleFileChange}
-                    className="border border-dashed border-gray-500 p-2"
-                />
-                <input type="hidden" name="userId" value={user.userId} />
-                {/* <div className="h-48">
-                    {avatar ? <p>{avatar.name}</p> : <p>No file uploaded</p>}
-                </div> */}
-                <Button type="submit" className="mt-4">
-                    Upload Avatar
-                </Button>
-            </form>
-        </div>
-    );
-}
+//     return (
+//         <div>
+//             <form action={uploadFile}>
+//                 <input
+//                     name="file"
+//                     type="file"
+//                     accept="image/*"
+//                     onChange={handleFileChange}
+//                     className="border border-dashed border-gray-500 p-2"
+//                 />
+//                 <input type="hidden" name="userId" value={user.userId} />
+//                 {/* <div className="h-48">
+//                     {avatar ? <p>{avatar.name}</p> : <p>No file uploaded</p>}
+//                 </div> */}
+//                 <Button type="submit" className="mt-4">
+//                     Upload Avatar
+//                 </Button>
+//             </form>
+//         </div>
+//     );
+// }
